11efc0c5cb8bbf25254524c6e4ef200040c5c303
sagorsohel <sohel19sagor@gmail.com>
2026-07-22 09:25:52 +0600

    feat: initialize tailwindcss configuration and disable dev indicators in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.0.103'],
+  devIndicators: {
+    appIsrStatus: false,
+  } as any,
 }
 
 export default nextConfig
